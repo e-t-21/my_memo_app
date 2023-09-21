cf29b487ee7ae33e3868f07ae291bc65f14bbf76
hatomugi21 <hatomugi.21@gmail.com>
2023-09-21 15:41:55 +0900

    認証処理の追加

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_migrate import Migrate
-from models import db
+from models import db, User
+from flask_login import LoginManager
 
 # ==================================================
 # Flask
@@ -12,6 +13,20 @@ app.config.from_object("config.Config")
 db.init_app(app)
 # マイグレーションとの紐づけ（Flaskとdb）
 migrate = Migrate(app, db)
+# LoginManagerインスタンス
+login_manager = LoginManager()
+# LoginManagerとFlaskとの紐づけ
+login_manager.init_app(app)
+# ログインが必要なページにアクセスしようとしたときに表示されるメッセージを変更
+login_manager.login_message = "認証していません：ログインしてください"
+# 未認証のユーザーがアクセスしようとした際に
+# リダイレクトされる関数名を設定する
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 # viewsのインポート
 from views import *
 
--- views.py
+++ views.py
@@ -1,13 +1,75 @@
 from flask import render_template, request, redirect, url_for, flash
 from app import app
-from models import db, Memo
-from forms import MemoForm
+
+from models import db, Memo, User
+from forms import MemoForm, LoginForm, SignUpForm
+from flask_login import login_user, logout_user, login_required #<= リスト11-8の追加
 
 # ==================================================
 # ルーティング
 # ==================================================
+# ログイン（Form使用）
+@app.route("/", methods=["GET", "POST"])
+def login():
+    # Formインスタンス生成
+    form = LoginForm()
+    if form.validate_on_submit():
+        # データ入力取得
+        username = form.username.data
+        password = form.password.data
+        # 対象User取得
+        user = User.query.filter_by(username=username).first()
+        # 認証判定
+        if user is not None and user.check_password(password):
+            # 成功
+            # 引数として渡されたuserオブジェクトを使用して、ユーザーをログイン状態にする
+            login_user(user)
+            # 画面遷移
+            return redirect(url_for("index"))
+        # 失敗
+        flash("認証不備です")
+    # GET時
+    # 画面遷移
+    return render_template("login_form.html", form=form)
+
+# ログアウト
+@app.route("/logout")
+@login_required     #<= リスト11-8の追加
+def logout():
+    # 現在ログインしているユーザーをログアウトする
+    logout_user()
+    # フラッシュメッセージ
+    flash("ログアウトしました")   
+    # 画面遷移
+    return redirect(url_for("login"))
+
+# サインアップ（Form使用）
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    # Formインスタンス生成
+    form = SignUpForm()
+    if form.validate_on_submit():
+        # データ入力取得
+        username = form.username.data
+        password = form.password.data
+        # モデルを生成
+        user = User(username=username)
+        # パスワードハッシュ化
+        user.set_password(password)
+        # 登録処理
+        db.session.add(user)
+        db.session.commit()
+        # フラッシュメッセージ
+        flash("ユーザー登録しました")  
+        # 画面遷移 
+        return redirect(url_for("login"))
+    # GET時
+    # 画面遷移
+    return render_template("register_form.html", form=form)
+
 # 一覧
 @app.route("/memo/")
+@login_required     #<= リスト11-8の追加
 def index():
     # メモ全件取得
     memos = Memo.query.all()
@@ -16,6 +78,7 @@ def index():
 
 # 登録（Form使用）
 @app.route("/memo/create", methods=["GET", "POST"])
+@login_required     #<= リスト11-8の追加
 def create():
     # Formインスタンス生成
     form = MemoForm()
@@ -37,6 +100,7 @@ def create():
 
 # 更新（Form使用）
 @app.route("/memo/update/<int:memo_id>", methods=["GET", "POST"])
+@login_required     #<= リスト11-8の追加
 def update(memo_id):
     # データベースからmemo_idに一致するメモを取得し、
     # 見つからない場合は404エラーを表示
@@ -59,6 +123,7 @@ def update(memo_id):
 
 # 削除
 @app.route("/memo/delete/<int:memo_id>")
+@login_required     #<= リスト11-8の追加
 def delete(memo_id):
     # データベースからmemo_idに一致するメモを取得し、
     # 見つからない場合は404エラーを表示
